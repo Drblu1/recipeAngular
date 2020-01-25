5c3af7ebe2ca8fa3bb2579e3e5c6357560470c54
Yann <yannyavo@gmail.com>
2020-01-25 17:41:11 +0100

    add ingredient from recipe to shopping list

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {ShoppingListService} from "../../shopping-list/shopping-list.service";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-details',
@@ -10,10 +12,13 @@ export class RecipeDetailsComponent implements OnInit {
 
   @Input() private recipe: Recipe;
 
-  constructor() {
+  constructor(private recipeService: RecipeService) {
   }
 
   ngOnInit() {
   }
 
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
 import {Ingredient} from "../shared/ingredient.model";
 import {EventEmitter} from "@angular/core";
+import {Recipe} from "../recipes/recipe.model";
 
 export class ShoppingListService {
 
-  ingredientAdded: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
+  ingredientAdded: EventEmitter<Ingredient[]> = new EventEmitter<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apple', 5),
@@ -16,6 +17,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient): void {
     this.ingredients.push(ingredient);
-    this.ingredientAdded.emit(ingredient);
+    this.ingredientAdded.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]): void {
+    this.ingredients.push(...ingredients);
+    this.ingredientAdded.emit(this.ingredients.slice())
   }
 }
