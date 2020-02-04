9f71fb03d1c44847c520915606816e38bc7811b2
yayavo <yann.yavo@sogeti.com>
2020-02-04 13:57:48 +0100

    Correct a fetch bug using resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import {ErrorPageComponent} from "./error-page/error-page.component";
 import {RecipeDetailsComponent} from "./recipes/recipe-details/recipe-details.component";
 import {RecipeStart} from "./recipes/no-recipe-selected/recipe-start.component";
 import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
+import {RecipesResolverService} from "./recipes/recipesResolver.service";
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes' , pathMatch: "full"},
   {path: 'recipes', component: RecipesComponent, children: [
       {path: '', component: RecipeStart},
       {path: 'new', component: RecipeEditComponent},
-      {path: ':id', component: RecipeDetailsComponent},
-      {path: ':id/edit', component: RecipeEditComponent}
+      {path: ':id', component: RecipeDetailsComponent, resolve: [RecipesResolverService]},
+      {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
     ]},
   {path: 'shopping-list', component: ShoppingListComponent},
   {path: '**', component: ErrorPageComponent}
