b673b0f55aa3c94aac0bfd20ed47f5d4cca2a3d8
Yann <yannyavo@gmail.com>
2020-02-15 17:26:17 +0100

    add auto login through localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import {Component} from '@angular/core';
-import {HeaderState} from './header/headerState.enum';
-import {Router, RouterModule} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./auth/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.autoLogin();
+  }
 
 }
