6acab061a79c6fca7a9ea395fa09f5a18fb3dcbb
Yann <yannyavo@gmail.com>
2020-02-16 18:01:31 +0100

    Preloaded lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import {RouterModule, Routes} from "@angular/router";
+import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {ErrorPageComponent} from "./error-page/error-page.component";
 
@@ -11,7 +11,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
