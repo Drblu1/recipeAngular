273ae5fb0c934dfd28a23b8732fc704794d312fb
Yann <yannyavo@gmail.com>
2020-02-16 15:48:11 +0100

    Make recipe feature into a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,43 +2,32 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
-import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
-import {RecipeDetailsComponent} from './recipes/recipe-details/recipe-details.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingListEditComponent} from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import {HeaderComponent} from './header/header.component';
-import {RecipesComponent} from './recipes/recipes.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from "./app-routing.module";
 import {ErrorPageComponent} from './error-page/error-page.component';
-import {RecipeStart} from './recipes/no-recipe-selected/recipe-start.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 import {RecipeService} from "./recipes/recipe.service";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from "./auth/auth.component";
 import {LoadingSpinnerComponent} from "./shared/loading-spinner/loading-spinner.component";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
+import {AlertComponent} from './shared/alert/alert.component';
+import {PlaceholderDirective} from './shared/placeholder/placeholder.directive';
+import {RecipesModules} from "./recipes/recipes.modules";
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailsComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
     HeaderComponent,
-    RecipesComponent,
     DropdownDirective,
     ErrorPageComponent,
-    RecipeStart,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -50,7 +39,8 @@ import { PlaceholderDirective } from './shared/placeholder/placeholder.directive
     RouterModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RecipesModules
   ],
   providers: [
     RecipeService,
